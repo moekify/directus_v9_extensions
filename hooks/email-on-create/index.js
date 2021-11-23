1daa0b829cc93fd27a77c16b0cba2a98e1f99dd2
Mirko Nitschke <mnitschke93@gmail.com>
2021-11-23 15:26:10 +0000

    Move to new hooks format of directus v9 stable

--- hooks/email-on-create/index.js
+++ hooks/email-on-create/index.js
@@ -1,37 +1,28 @@
-module.exports = function registerHook({ services, exceptions }) {
+module.exports = function registerHook({ action }, { services, exceptions }) {
   const { MailService } = services;
   const { ServiceUnavailableException } = exceptions;
+  action("items.create", async ({ item, collection, schema, payload }) => {
+    const mailService = new MailService({ schema });
+    const subject = "New " + collection + " created: " + payload.name;
+    const link = "directusbase/collections/" + collection + "/" + item;
 
-  return {
-    "items.create": async function ({
-      event,
-      item,
-      collection,
-      schema,
-      payload,
-    }) {
-      const mailService = new MailService({ schema });
-      const subject = 'New ' + collection + ' created: ' + payload.name;
-      const link = 'directusbase/collections/' + collection + '/' + item
-
-      if (collection === "events") {
-        try {
-          await mailService.send({
-            subject: subject,
-            to: ["email"],
-            template: {
-              name: "new-event",
-              data: {
-                name: payload.name,
-                payload: payload,
-                link: link
-              },
+    if (collection === "events") {
+      try {
+        await mailService.send({
+          subject: subject,
+          to: ["email"],
+          template: {
+            name: "new-event",
+            data: {
+              name: payload.name,
+              payload: payload,
+              link: link,
             },
-          });
-        } catch (error) {
-          throw new ServiceUnavailableException(error);
-        }
+          },
+        });
+      } catch (error) {
+        throw new ServiceUnavailableException(error);
       }
-    },
-  };
+    }
+  });
 };
